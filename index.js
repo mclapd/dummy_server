d326fbe44c3253ef98549684a60b6da6e185eb86
one_punch_man <ckim@banlaw.com>
2024-02-22 11:32:06 +1100

    Add authentication and authorization functionality

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const adminRouter = require("./routes/admin");
+const authRouter = require("./routes/auth");
+// const productRouter = require("./routes/product");
+// const userRouter = require("./routes/user");
+
+require("dotenv").config();
+AmazonDB_Uri = process.env.MONGODB_URI;
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+const DB = AmazonDB_Uri;
+
+app.use(express.json());
+app.use(authRouter);
+// app.use(adminRouter);
+// app.use(productRouter);
+// app.use(userRouter);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
